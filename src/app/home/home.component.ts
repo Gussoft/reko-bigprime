59abb05982f0b2df8a33b9dc33755ce699551477
Guss <analista2@bigprime.pe>
2021-08-12 15:31:37 -0500

    otro

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
     this.loadScript('https://code.jquery.com/jquery-2.2.4.min.js','anonymous',null);
     this.loadScript('https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js','anonymous','sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo');
     this.loadScript('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js','anonymous','sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6');
-    this.loadScript('http://localhost:9006/api/bigface?dni=46191069&externalId=214&uuid=8e90deb67f3c4e21b2cc6d30736f732a',null,null);
+    this.loadScript('http://localhost:9006/api/bigface?dni=46191069&externalId=215&uuid=8e90deb67f3c4e21b2cc6d30736f732a',null,null);
 
   }
 
